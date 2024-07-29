5c8c2e0483bfa258c47c4591e1c45070e9129500
Kiran <kiran.kp2004@gmail.com>
2024-07-29 16:45:56 +0530

    React-project-Assignment

--- Day_3/todoapp/src/components/Item.js
+++ Day_3/todoapp/src/components/Item.js
@@ -1,17 +1,7 @@
-function Item({item}){
-    return(
-        <li className="todo_item">
-            <button className="todos_items_left">
-                <p>{item?.title}</p>
-            </button>
-            <div>
-                <button><span className="visually-hidden">
-                    Edit</span></button> 
-                <button><span className="visually-hidden">
-                Delete</span></button>           
-            </div>
-        </li>
-    )
+import React from "react"
+
+function Item(){
+    return(<div> </div>)
 }
 
 export default Item
