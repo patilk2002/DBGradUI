e9d665687e58520d4d0b6a305d5a26ac3c937958
Kiran <kiran.kp2004@gmail.com>
2024-07-30 12:31:38 +0530

    Redux-edit optio

--- Redux/todoapp/src/todo.js
+++ Redux/todoapp/src/todo.js
@@ -1,50 +1,84 @@
-import React,{useState} from 'react'
-import {useSelector, useDispatch} from 'react-redux'
-import { addTodo, deleteTodo, toggleComplete } from './todoSlice'
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { addTodo, deleteTodo, toggleComplete, editTodo } from './todoSlice';
+
 const Todo = () => {
-    const [text,setText] = useState("")
-    const todos = useSelector((state)=> state.todos)
-    const dispatch = useDispatch()
+  const [text, setText] = useState("");
+  const [editingId, setEditingId] = useState(null);
+  const [editText, setEditText] = useState("");
+  const todos = useSelector((state) => state.todos);
+  const dispatch = useDispatch();
 
-
-const handleInputChange= (e)=>{
+  const handleInputChange = (e) => {
     setText(e.target.value);
-}
+  };
 
-const handleAddTo = () => {
-    if(text){
-        dispatch(addTodo(text))
-        setText("")
+  const handleEditInputChange = (e) => {
+    setEditText(e.target.value);
+  };
+
+  const handleAddTodo = () => {
+    if (text) {
+      dispatch(addTodo(text));
+      setText("");
     }
-}
+  };
 
-const handleToggleComplete = (id)=>{
-    dispatch(toggleComplete(id))
-}
+  const handleEditTodo = (id, text) => {
+    setEditingId(id);
+    setEditText(text);
+  };
 
-const handleDeleteTodo = (id)=>{
-    dispatch(deleteTodo(id))
-}
+  const handleSaveEdit = (id) => {
+    dispatch(editTodo({ id, text: editText }));
+    setEditingId(null);
+    setEditText("");
+  };
 
-return (
+  const handleToggleComplete = (id) => {
+    dispatch(toggleComplete(id));
+  };
 
+  const handleDeleteTodo = (id) => {
+    dispatch(deleteTodo(id));
+  };
+
+  return (
     <div>
-        <input type="text" value={text} onChange={handleInputChange} />{ " "}
-        <button onClick={handleAddTo}> Add Todo </button>{ " "}
-        <ul>
-        { " "}
-        {todos.map((todo)=>(
-            <li key={todo.id}
-            style={{textDecoration:todo.completed?"line-through":"none"}}
-            >{todo.text}{ " "}
-            <button onClick={()=>handleToggleComplete(todo.id)}>{ " "} {todo.completed?"Mark In Complete":"Mark Complete"}{ " "}</button>
-            <button onClick={()=>handleDeleteTodo(todo.id)}>Delete</button>{ " "}
-            </li>
-        ))}{ " "}
-        </ul>{ " "}
+      <input type="text" value={text} onChange={handleInputChange} />{" "}
+      <button onClick={handleAddTodo}>Add Todo</button>{" "}
+      <ul>
+        {" "}
+        {todos.map((todo) => (
+          <li
+            key={todo.id}
+            style={{ textDecoration: todo.completed ? "line-through" : "none" }}
+          >
+            {editingId === todo.id ? (
+              <>
+                <input
+                  type="text"
+                  value={editText}
+                  onChange={handleEditInputChange}
+                />
+                <button onClick={() => handleSaveEdit(todo.id)}>Save</button>
+                <button onClick={() => setEditingId(null)}>Cancel</button>
+              </>
+            ) : (
+              <>
+                {todo.text}{" "}
+                <button onClick={() => handleToggleComplete(todo.id)}>
+                  {todo.completed ? "Mark Incomplete" : "Mark Complete"}{" "}
+                </button>
+                <button onClick={() => handleDeleteTodo(todo.id)}>Delete</button>{" "}
+                <button onClick={() => handleEditTodo(todo.id, todo.text)}>Edit</button>
+              </>
+            )}
+          </li>
+        ))}{" "}
+      </ul>{" "}
     </div>
-)
+  );
+};
 
-}
-
-export default Todo
+export default Todo;
